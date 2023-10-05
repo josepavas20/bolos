e2a14bdcc56b645409281c1593a04223d75b470e
jose20pavas <jomapago05@gmail.com>
2023-10-05 09:49:26 -0500

    Initial commit

---
+++ bolos.py
@@ -0,0 +1,18 @@
+class Game:
+    def __init__(self):
+
+    def roll(self, pins):
+        self.pins: int = pins
+
+    def score(self)->int:
+        pass
+
+
+class Frame:
+
+    def add_roll(self):
+        self.pins: int
+        pass
+    def score(self)->:
+        pass
+    
